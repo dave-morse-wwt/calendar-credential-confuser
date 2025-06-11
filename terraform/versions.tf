cc51cef7fe770108c6561e0e5b1420aa2262b2e8
Dave Morse <morsed@wwt.com>
2025-06-11 11:26:56 -0500

    copypasta from AKS K8S Quickstart tutorial

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,9 +2,25 @@ terraform {
   required_version = ">= 1.12.1"
 
   required_providers {
+    # The below versions are from the tutorial at https://learn.microsoft.com/en-us/azure/aks/learn/quick-kubernetes-deploy-terraform?pivots=development-environment-azure-cli
+    # And are not the latest. 
+
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.31"
+      # version = "~> 4.31" # latest rn
+      version = "~>3.0"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
     }
   }
 }
