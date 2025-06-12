38309014ee82e73f4ab2dfb6a23c84d4b97798a5
Dave Morse <morsed@wwt.com>
2025-06-12 11:13:09 -0500

    tweak cluster tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   default_node_pool {
     name       = "agentpool"
-    vm_size    = "Standard_B1ms"
+    vm_size    = "Standard_B2s"
     node_count = var.node_count
   }
   # linux_profile {
