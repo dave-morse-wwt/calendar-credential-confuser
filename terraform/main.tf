ceff285302daec990c5698ef0f8dd72fcfc6d221
Dave Morse <morsed@wwt.com>
2025-06-30 14:16:26 -0500

    add vnet and postgres

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,44 @@ resource "azurerm_resource_group" "rg" {
   name     = random_pet.rg_name.id
 }
 
+
+# --- VNet and Subnets ---
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-shared"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+
+resource "azurerm_subnet" "postgres_subnet" {
+  name                 = "postgres-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+  delegation {
+    name = "delegation"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+
+# --- AKS ---
+
 resource "random_pet" "azurerm_kubernetes_cluster_name" {
   prefix = "cluster"
 }
@@ -33,6 +71,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     name       = "agentpool"
     vm_size    = "Standard_B2s"
     node_count = var.node_count
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
   }
   # linux_profile {
   #   admin_username = var.username
@@ -42,7 +81,23 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   #   }
   # }
   network_profile {
-    network_plugin    = "kubenet"
+    network_plugin    = "azure"
+    dns_service_ip = "10.0.10.10"
+    service_cidr = "10.0.10.0/24"
     load_balancer_sku = "standard"
   }
+}
+
+
+# --- Private DNS Zone ---
+resource "azurerm_private_dns_zone" "postgres_zone" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
+  name                  = "dns-link"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.postgres_zone.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
 }
